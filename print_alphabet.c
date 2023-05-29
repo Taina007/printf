a57a76c4e686fb48f3177d9e0a449d8bb81aafa6
Taina007 <tainachan067@gmail.com>
2023-05-29 12:25:10 -0700

    print numbers

--- print_alphabet.c
+++ print_alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-***
  * print_string - loops through a string and prints
  * every character
  * @l: va_list arguments from _printf
@@ -25,7 +24,7 @@ int print_string(va_list l, flags_t *f)
 * @f: pointer to the struct flags that determines
 * if a flag is passed to _printf
 * Return: number of char printed
-*/ 
+*/
 int print_char(va_list l, flags_t *f)
 {
 	(void)f;
